a9a2eb983fa53cea0ed6f0f40df18973a9a6c0e2
Jared W <jwyles@atlassian.com>
2011-04-08 15:31:09 +1000

    adding the first of the tasks

---
+++ src/main/java/com/atlassian/bamboo/plugins/GitBranch.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.atlassian.bamboo.plugins;
+
+/**
+ *
+ * @author jwyles
+ */
+public class GitBranch {
+
+}
